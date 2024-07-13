aed968986d0243b33d7e43498f015c658b214d4a
Zaina15 <136789779+Zaina15@users.noreply.github.com>
2024-07-13 21:01:37 +0530

    temp

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -43,100 +43,46 @@ export default function Header({
   },
 }: Nav) {
   return (
-    <nav class="drawer drawer-end">
-      <input id="mobile-drawer-nav" type="checkbox" class="drawer-toggle" />
-
-      {/* main content */}
-      <div class="drawer-content container lg:px-0 px-4 flex gap-8 items-center justify-between py-4">
-        <a href="/">
-          <Image src={logo.src || ""} width={100} height={28} alt={logo.alt} />
-        </a>
-
-        <div class="hidden items-center justify-between lg:flex w-full">
-          <ul class="flex">
-            {navigation.links.map((link) => (
-              <li>
-                <a
-                  href={link.url}
-                  aria-label={link.label}
-                  class="link no-underline hover:underline p-4"
-                >
-                  {link.label}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <ul class="flex gap-3">
-            {navigation.buttons?.map((item) => (
-              <a
-                key={item?.id}
-                id={item?.id}
-                href={item?.href ?? "#"}
-                target={item?.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary ${
-                  item.outline && "btn-outline"
-                }`}
-              >
-                {item?.text}
-              </a>
-            ))}
-          </ul>
-        </div>
-
-        <label
-          htmlFor="mobile-drawer-nav"
-          class="flex lg:hidden btn btn-ghost drawer-button"
-        >
-          <Icon id="Bars3" size={24} strokeWidth={0.1} />
-        </label>
-      </div>
-
-      {/* sidebar */}
-      <aside class="drawer-side z-50">
-        {/* Close when clicking on overlay */}
-        <label
-          htmlFor="mobile-drawer-nav"
-          aria-label="close sidebar"
-          class="drawer-overlay"
-        />
-
-        <div class="flex flex-col gap-8 min-h-full w-80 bg-base-100 text-base-content">
-          <a class="p-4" href="/">
-            <Image
-              src={logo.src || ""}
-              width={100}
-              height={28}
-              alt={logo.alt}
-            />
+    <nav
+  class="block w-full px-6 py-3 mx-auto text-white bg-[#045F73] bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200">
+  <div class="flex items-center max-w-screen-xl justify-between text-blue-gray-900">
+    <Image
+        class="h-20 w-20"
+        src="https://i.ibb.co/KWg2d5j/logo.png"
+        alt=""
+        decoding="async"
+        loading="lazy"
+      />
+    <div class="hidden lg:block">
+      <ul class="flex flex-col gap-2 my-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+        <li class="block p-1 font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
+          <a href="#" class="flex items-center transition-colors hover:text-blue-500">
+            Recipe
           </a>
-
-          <ul class="menu">
-            {navigation?.links.map((link) => (
-              <li>
-                <a href={link.url} aria-label={link.label}>
-                  {link.label}
-                </a>
-              </li>
-            ))}
-          </ul>
-
-          <ul class="p-4 flex items-center gap-3">
-            {navigation.buttons?.map((item) => (
-              <a
-                key={item?.id}
-                id={item?.id}
-                href={item?.href ?? "#"}
-                target={item?.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary ${
-                  item.outline && "btn-outline"
-                }`}
-              >
-                {item?.text}
-              </a>
-            ))}
-          </ul>
-        </div>
-      </aside>
-    </nav>
+        </li>
+        <li class="block p-1 font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
+          <a href="#" class="flex items-center transition-colors hover:text-blue-500">
+            Personalized search
+          </a>
+        </li>
+        <li class="block p-1 font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
+          <a href="#" class="flex items-center transition-colors hover:text-blue-500">
+            Saved
+          </a>
+        </li>
+        <li class="block p-1 font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
+          <a href="#" class="flex items-center transition-colors hover:text-blue-500">
+            About us
+          </a>
+        </li>
+        <button
+          class="select-none rounded-lg bg-yellow py-4 px-6 text-center font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20"
+          type="button">
+          Search
+        </button>
+      </ul>
+    </div>
+  </div>
+</nav>
   );
 }
